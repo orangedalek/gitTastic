c0dadb8fc40aeaa6d8a650c0faddebd7466dccbf
jennie snider <jenniesnider@jennies-MacBook-Pro.local>
2017-06-25 15:18:20 -0700

    adding topics array

---
+++ assets/thingy.js
@@ -0,0 +1,4 @@
+//adding array of strings for bands in my itunes that I have seen live
+
+var topics = ["AFI", "Arcade Fire", "Bauhaus", "The Big Pink", "Blonde Redhead", "Bear in Heaven", "Cat Power", "Dead Milkmen", "Electric Six", "The Faint", "Front 242", "Gary Numan", "Grinderman", "The Jesus and Mary Chain", "Jenny Lewis and the Watson Twins", "Ladytron", "Leftover Crack", "Le Tigre", "The Mountain Goats", "My Bloody Valentine", "My Life With the Thrill Kill Kult", "Neutral Milk Hotel", "Nick Cave and the Bad Seeds", "Pixies", "Rilo Kiley", "Sisters of Mercy", "Spoon", "Tame Impala", "Tenacious D", "Wolf Parade"];
+
